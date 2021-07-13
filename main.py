3ea3a5e8d73900a5e960ecee6fbb4d16ba2e37ae
sam1101-sys <55644100+sam1101-sys@users.noreply.github.com>
2021-07-13 10:25:32 +0530

    Treasure-Island

--- main.py
+++ main.py
@@ -34,8 +34,6 @@ print('''
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
-
 choice1=input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
 if choice1 == "left" or choice1 =="l":
   choice2=input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
